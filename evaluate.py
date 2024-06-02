Training : add hyperparameter tuning + fix metrics name precision + discriminate test and val

evaluate.py
@@ -30,11 +30,6 @@ class Evaluator:
         for item in st.session_state.keys():
             if item.endswith('labels'):
                 self.labels[item] = st.session_state[item]
-    # def truncate(self, n):
-    #     if n != 0 and not np.isnan(n):
-    #         return int(n * 100) / 100
-    #     else:
-    #         return n
     def correction(self, correct_labels, test_labels):
         self.erreurs = [i for i, l in enumerate(correct_labels) if l != test_labels[i]]
         self.nb_erreurs = len(self.erreurs)
@@ -45,10 +40,10 @@ class Evaluator:
     def metrics_calculation(self):
         self.recall = [self.cm[i, i] / self.cm[i, :].sum() for i in range(0,10)]
         print('metrics calculation')
-        self.accuracy = [self.cm[i, i]/self.cm[:, i].sum() for i in range(0,10)]
+        self.precision = [self.cm[i, i]/self.cm[:, i].sum() for i in range(0,10)]
         FP_error = np.array([self.cm[0, i] for i in range(1, 10)])
         FN_error = np.array([self.cm[i, 0] for i in range(1, 10)])
-        # /!\ delete uses the index in the np.arange(1,10) not from the self.cm, which is where the i-1 comes from
+        # delete uses the index in the np.arange(1,10) not from the self.cm, which is where the i-1 comes from
         FP_symbol = np.array([self.cm[np.delete(np.arange(1, 10), i - 1), i].sum() for i in range(1, 10)])
         FN_symbol = np.array([self.cm[i, np.delete(np.arange(1, 10), i - 1)].sum() for i in range(1, 10)])
         FP_total = FP_error + FP_symbol
@@ -62,7 +57,7 @@ class Evaluator:
         plt.ylabel('True')
         return plt
     def metrics_df(self):
-        df = pd.DataFrame({'Accuracy': self.accuracy, 'Recall': self.recall},
+        df = pd.DataFrame({'Precision': self.precision, 'Recall': self.recall},
                           index=range(0, 10))
         return df.transpose()
     def metrics_plot(self):

tcutil.py
@@ -32,11 +32,9 @@ def sidebar_progress():
     if 'uploaded_file_name' in st.session_state:
         st.sidebar.success(f"Fichier analysé : {repr(st.session_state['uploaded_file_name'])}")
     else:
-        st.sidebar.success("Start by extracting your symbols")
-    if 'roi_symbols' in st.session_state:
-        st.sidebar.success(f"Symboles extraits : {len(st.session_state['roi_symbols'])}")
-    if 'annot_labels' in st.session_state:
-        st.sidebar.success('Symboles annotés')
+        st.sidebar.success("Aucun fichier uploadé")
+    if 'ex_roi_symbols' in st.session_state:
+        st.sidebar.success(f"Symboles extraits : {len(st.session_state['ex_roi_symbols'])}")
 
 def sidebar_legend():
     legend = np.loadtxt("data_resultsheets/legend_pictures.csv", delimiter=',')

train.py
@@ -34,6 +34,7 @@ class Trainer:
             self.load_dataset(datapath)
             self.split_labels()
         self.redimension_pict()
+        self.remove_zero()
     def load_dataset(self, datapath):
         '''
         Used when loading a saved dataset (pict + label)
@@ -52,15 +53,18 @@ class Trainer:
         '''
         Used when loading a saved dataset (pict + label)
         '''
-        keeper_i = np.where(self.full_da[:, -1] != 0)[0]
-        self.full_da = self.full_da[keeper_i, :]
         self.pict, self.label = np.split(self.full_da, [ext_w*ext_h], axis = 1)
         self.label = self.label.reshape((len(self.label),)).astype(np.int32)
         self.pict = self.pict.reshape((len(self.pict), ext_w, ext_h))
+    def remove_zero(self):
+        keeper_i = np.where(self.full_da[:, -1] != 0)[0]
+        self.label_zeros = self.label
+        self.pict_zeros = self.pict_redim
+        self.label = self.label[keeper_i]
+        self.pict_redim = self.pict_redim[keeper_i, :, :]
     def redimension_pict(self, size=(28, 28)):
         self.size = size
         self.pict = self.pict.astype(np.float32)
-        self.pict = self.pict.reshape(-1, self.pict.shape[1], self.pict.shape[2], 1)
         self.pict_redim = np.empty((len(self.pict), self.size[0], self.size[1]))
         for i in range(len(self.pict)):
             img_resized = cv.resize(self.pict[i], self.size, interpolation=cv.INTER_AREA)
@@ -106,5 +110,11 @@ class Trainer:
         plt.show()
 
 if __name__ == "__main__":
-    t = Trainer(None, "data_resultsheets/Test/")
-    t.visualize_random_samples(t.label)
+    t = Trainer(datapath="data_resultsheets/Test/")
+    print(t.pict.shape)
+    print(t.pict_redim.shape)
+    print(t.pict_zeros.shape)
+    print(t.label.shape)
+    print(t.label_zeros.shape)
+    print(t.pict_redim.min() + t.pict_redim.max())
+    print(t.pict_zeros.min() + t.pict_zeros.max())